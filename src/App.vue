db477c2097ce30c61b1d722ab87af2f38a2bfb0c
kbdatasystemsindonesia <85105168+trinandairvan123@users.noreply.github.com>
2022-12-25 16:02:22 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <input v-model="text" type="text" placeholder="Masukkan teks" />
+    <button @click="generateAvatar">Generate Avatar</button>
+    <img v-if="avatarUrl" :src="avatarUrl" alt="Avatar" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: "",
+      avatarUrl: ""
+    };
+  },
+  methods: {
+    generateAvatar() {
+      this.avatarUrl = `https://robohash.org/${this.text}`;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  background-color: #4caf50;
+  color: #fff;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #46a049;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin-top: 10px;
 }
 </style>
